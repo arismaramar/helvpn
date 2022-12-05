1538de9b19a40709be2b22159c68fa2b2763c522
brjalan <74614880+arismaramar@users.noreply.github.com>
2022-12-06 01:32:55 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -43,18 +43,18 @@ ORANGE='\033[0;33m'
 LIGHT='\033[0;37m'
 # VPS Information
 #Domain
-domain=$(cat /etc/xray/domain)
+#domain=$(cat /etc/xray/domain)
 #Status certificate
-modifyTime=$(stat $HOME/.acme.sh/${domain}_ecc/${domain}.key | sed -n '7,6p' | awk '{print $2" "$3" "$4" "$5}')
-modifyTime1=$(date +%s -d "${modifyTime}")
-currentTime=$(date +%s)
-stampDiff=$(expr ${currentTime} - ${modifyTime1})
-days=$(expr ${stampDiff} / 86400)
-remainingDays=$(expr 90 - ${days})
-tlsStatus=${remainingDays}
-if [[ ${remainingDays} -le 0 ]]; then
-	tlsStatus="expired"
-fi
+#modifyTime=$(stat $HOME/.acme.sh/${domain}_ecc/${domain}.key | sed -n '7,6p' | awk '{print $2" "$3" "$4" "$5}')
+#modifyTime1=$(date +%s -d "${modifyTime}")
+#currentTime=$(date +%s)
+#stampDiff=$(expr ${currentTime} - ${modifyTime1})
+#days=$(expr ${stampDiff} / 86400)
+#remainingDays=$(expr 90 - ${days})
+#tlsStatus=${remainingDays}
+#if [[ ${remainingDays} -le 0 ]]; then
+#	tlsStatus="expired"
+#fi
 # OS Uptime
 uptime="$(uptime -p | cut -d " " -f 2-10)"
 # Download
